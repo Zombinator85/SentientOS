2a9e6f21be744b1e67bfc97a8e85729f9b6c6dc6
Allen Murphy <89659740+Zombinator85@users.noreply.github.com>
2025-06-09 17:00:59 -0400

    docs(site): add Sphinx auto-docs and GitHub Pages deploy

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'SentientOS'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',
+    'myst_parser',
+]
+
+html_theme = 'furo'
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
