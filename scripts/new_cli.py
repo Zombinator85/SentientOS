83ae36eefffda38770bceff5be1b1136b2248b23
Allen Murphy <89659740+Zombinator85@users.noreply.github.com>
2025-06-10 22:01:36 -0400

    Fix banner order in new_cli

--- scripts/new_cli.py
+++ scripts/new_cli.py
@@ -1,5 +1,6 @@
 """Sanctuary Privilege Ritual: Do not remove. See doctrine for details."""
 from __future__ import annotations
+
 from admin_utils import require_admin_banner, require_lumos_approval
 
 import argparse
