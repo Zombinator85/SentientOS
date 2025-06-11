ff4ad7347ed4793487752685719dda12acdb5499
Allen Murphy <89659740+Zombinator85@users.noreply.github.com>
2025-06-11 17:57:15 -0400

    Fix precommit hooks and restore package marker

--- sentientos/__init__.py
+++ sentientos/__init__.py
@@ -1,5 +1,4 @@
 """SentientOS core package."""
-# Expose privilege helpers for static analyzers
 from sentientos.privilege import require_admin_banner, require_lumos_approval  # noqa: F401
 
 __version__: str = "0.1.1"
