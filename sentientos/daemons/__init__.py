658a83b08bf2b78c988b22d335552d55d96e1bcf
Allen Murphy <89659740+Zombinator85@users.noreply.github.com>
2025-10-28 15:15:50 -0400

    Add HungryEyes sentinel training pipeline

--- sentientos/daemons/__init__.py
+++ sentientos/daemons/__init__.py
@@ -2,11 +2,12 @@
 
 from __future__ import annotations
 
-from . import driver_manager, monitoring_daemon, pulse_bus, pulse_federation
+from . import driver_manager, hungry_eyes, monitoring_daemon, pulse_bus, pulse_federation
 
 __all__ = [
     "pulse_bus",
     "pulse_federation",
     "monitoring_daemon",
     "driver_manager",
+    "hungry_eyes",
 ]
